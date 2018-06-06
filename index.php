89337b5860e80852c4098f7ef3627b14c723a367
Mike Angelo Saraus <mail@mikesaraus.tk>
2018-06-06 09:27:45 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+
+<!doctype html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!-- Begin Jekyll SEO tag v2.4.0 -->
+<title>Mike Angelo Saraus | Saraus, Mike Angelo</title>
+<meta name="generator" content="Jekyll v3.7.3" />
+<meta property="og:title" content="Mike Angelo Saraus" />
+<meta property="og:locale" content="en_US" />
+<meta name="description" content="Make things as simple as possible, but not simpler." />
+<meta property="og:description" content="Make things as simple as possible, but not simpler." />
+<link rel="canonical" href="https://mikesaraus.github.io/" />
+<meta property="og:url" content="https://mikesaraus.github.io/" />
+<meta property="og:site_name" content="Saraus, Mike Angelo" />
+<script type="application/ld+json">
+{"name":"Saraus, Mike Angelo","description":"Make things as simple as possible, but not simpler.","@type":"WebSite","url":"https://mikesaraus.github.io/","headline":"Mike Angelo Saraus","@context":"http://schema.org"}</script>
+<!-- End Jekyll SEO tag -->
+
+    <link rel="stylesheet" href="/assets/css/style.css?v=d9d04d1595127e60717b7d8553b72239a109e9b3">
+    <script src="/assets/js/scale.fix.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <h1 class="header">Saraus, Mike Angelo</h1>
+        <p class="header">Make things as simple as possible, but not simpler.</p>
+
+        <ul>
+          
+          <li><a class="buttons github" href="https://github.com/mikesaraus/mikesaraus.github.io">View On GitHub</a></li>
+        </ul>
+
+        
+
+        
+          <ul>
+            <li><a class="buttons github" href="https://github.com/mikesaraus">GitHub Profile</a></li>
+          </ul>
+        
+      </header>
+
+      <section>
+        <h2 id="mike-angelo-saraus">Mike Angelo Saraus</h2>
+
+<blockquote>
+  <blockquote>
+    <p>Feel free to browse my projects.</p>
+  </blockquote>
+</blockquote>
+
+      </section>
+
+      <footer>
+        <p><small>Hosted on <a href="https://pages.github.com">GitHub Pages</a> using the Dinky theme</small></p>
+      </footer>
+    </div>
+    <!--[if !IE]><script>fixScale(document);</script><![endif]-->
+    
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-120448615-2', 'auto');
+        ga('send', 'pageview');
+      </script>
+    
+  </body>
+</html>
